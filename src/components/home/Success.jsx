5ec39b7273425106a1112d541bb25d2b970195b1
Javier Lagunes <mjavlagunes@gmail.com>
2024-12-14 09:18:13 -0600

    Changed style for carousel in success section

--- src/components/home/Success.jsx
+++ src/components/home/Success.jsx
@@ -93,113 +93,121 @@ const Success = () => {
 */
   }
   return (
-    <div id='carouselExampleControls' className='carousel'>
-      <div className='carousel-inner' ref={carouselInner}>
-        <div className='carousel-item active'>
-          <div className='card'>
-            <div className='img-wrapper'>
-              <img
-                src='https://editorialtelevisa.brightspotcdn.com/5d/22/7b517e1242d78f28df597461df79/cupcakes-de-unicornio-receta-facil-y-rapida.jpg'
-                className='d-block w-100'
-                alt='...'
-              />
+    <div className='success'>
+      <div id='carouselExampleControls' className='carousel'>
+        <div className='carousel-inner' ref={carouselInner}>
+          <div className='carousel-item active'>
+            <div className='card'>
+              <div className='img-wrapper'>
+                <img
+                  src='https://editorialtelevisa.brightspotcdn.com/5d/22/7b517e1242d78f28df597461df79/cupcakes-de-unicornio-receta-facil-y-rapida.jpg'
+                  className='d-block w-100'
+                  alt='...'
+                />
+              </div>
+              <div className='card-body'>
+                <h5 className='card-title'>Caso de éxito 1</h5>
+                <p className='card-text'>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href='/case' className='btn btn-primary'>
+                  Ver más
+                </a>
+              </div>
             </div>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title 1</h5>
-              <p className='card-text'>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href='/case' className='btn btn-primary'>
-                Ver más
-              </a>
+          </div>
+          <div className='carousel-item'>
+            <div className='card'>
+              <div className='img-wrapper'>
+                <img
+                  src='https://cdn0.uncomo.com/es/posts/3/9/6/como_hacer_cupcakes_glaseados_6693_orig.jpg'
+                  className='d-block w-100'
+                  alt='...'
+                />
+              </div>
+              <div className='card-body'>
+                <h5 className='card-title'>Caso de éxito 2</h5>
+                <p className='card-text'>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href='#' className='btn btn-primary'>
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='carousel-item'>
+            <div className='card'>
+              <div className='img-wrapper'>
+                <img
+                  src='https://www.lacostena.com.mx/media/thumbnail/uploads/Recipes/postre/32_cupcakes_de_nata.jpg.1920x800_q85_center.jpg'
+                  className='d-block w-100'
+                  alt='...'
+                />
+              </div>
+              <div className='card-body'>
+                <h5 className='card-title'>Caso de éxito 3</h5>
+                <p className='card-text'>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href='#' className='btn btn-primary'>
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='carousel-item'>
+            <div className='card'>
+              <div className='img-wrapper'>
+                <img
+                  src='https://www.lacostena.com.mx/media/thumbnail/uploads/Recipes/postre/32_cupcakes_de_nata.jpg.1920x800_q85_center.jpg'
+                  className='d-block w-100'
+                  alt='...'
+                />
+              </div>
+              <div className='card-body'>
+                <h5 className='card-title'>Caso de éxito 4</h5>
+                <p className='card-text'>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href='#' className='btn btn-primary'>
+                  Ver más
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className='carousel-item'>
-          <div className='card'>
-            <div className='img-wrapper'>
-              <img
-                src='https://cdn0.uncomo.com/es/posts/3/9/6/como_hacer_cupcakes_glaseados_6693_orig.jpg'
-                className='d-block w-100'
-                alt='...'
-              />
-            </div>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title 2</h5>
-              <p className='card-text'>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href='#' className='btn btn-primary'>
-                Ver más
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className='carousel-item'>
-          <div className='card'>
-            <div className='img-wrapper'>
-              <img
-                src='https://www.lacostena.com.mx/media/thumbnail/uploads/Recipes/postre/32_cupcakes_de_nata.jpg.1920x800_q85_center.jpg'
-                className='d-block w-100'
-                alt='...'
-              />
-            </div>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title 3</h5>
-              <p className='card-text'>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href='#' className='btn btn-primary'>
-                Ver más
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className='carousel-item'>
-          <div className='card'>
-            <div className='img-wrapper'>
-              <img
-                src='https://www.lacostena.com.mx/media/thumbnail/uploads/Recipes/postre/32_cupcakes_de_nata.jpg.1920x800_q85_center.jpg'
-                className='d-block w-100'
-                alt='...'
-              />
-            </div>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title 4</h5>
-              <p className='card-text'>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href='#' className='btn btn-primary'>
-                Ver más
-              </a>
-            </div>
-          </div>
-        </div>
+        <button
+          className='carousel-control-prev'
+          type='button'
+          data-bs-target='#carouselExampleControls'
+          data-bs-slide='prev'
+          onClick={handlePrevClick}
+        >
+          <span
+            className='carousel-control-prev-icon'
+            aria-hidden='true'
+          ></span>
+          <span className='visually-hidden'>Previous</span>
+        </button>
+        <button
+          className='carousel-control-next'
+          type='button'
+          data-bs-target='#carouselExampleControls'
+          data-bs-slide='next'
+          onClick={handleNextClick}
+        >
+          <span
+            className='carousel-control-next-icon'
+            aria-hidden='true'
+          ></span>
+          <span className='visually-hidden'>Next</span>
+        </button>
       </div>
-      <button
-        className='carousel-control-prev'
-        type='button'
-        data-bs-target='#carouselExampleControls'
-        data-bs-slide='prev'
-        onClick={handlePrevClick}
-      >
-        <span className='carousel-control-prev-icon' aria-hidden='true'></span>
-        <span className='visually-hidden'>Previous</span>
-      </button>
-      <button
-        className='carousel-control-next'
-        type='button'
-        data-bs-target='#carouselExampleControls'
-        data-bs-slide='next'
-        onClick={handleNextClick}
-      >
-        <span className='carousel-control-next-icon' aria-hidden='true'></span>
-        <span className='visually-hidden'>Next</span>
-      </button>
     </div>
     /*<div className="carousel-container">
       <div className="carousel-track">
