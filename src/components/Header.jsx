ef3a9a3d7334144572e3d3abfd05444163f1d4fa
Javier Lagunes <mjavlagunes@gmail.com>
2025-01-22 09:14:20 -0600

    Changes to color on header and footer, blog configuration updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState, useEffect, forwardRef } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import info from '@/content/header.json'
 
 const Header = forwardRef((props, ref) => {
@@ -58,17 +58,17 @@ const Header = forwardRef((props, ref) => {
 
   return (
     <nav
-      className='navbar navbar-expand-lg navbar-dark bg-secondary py-1 px-5 container-fluid d-flex justify-content-between align-items-center content fixed-top'
+      className='navbar navbar-expand-lg navbar-dark bg-dark-subtle py-1 px-5 container-fluid d-flex justify-content-between align-items-center content fixed-top'
       ref={ref}
     >
-      <NavLink to='/' className='my-2 col-8 col-sm-7 col-lg-3'>
+      <Link to='/' className='my-2 col-8 col-sm-7 col-lg-3'>
         <img
           src={ImportDrivePhoto(info.data[0].logo, 600)}
           alt={info.data[0].name}
           className='w-100'
           style={{ width: '400px' }}
         />
-      </NavLink>
+      </Link>
 
       <button
         className='navbar-toggler'
@@ -84,7 +84,7 @@ const Header = forwardRef((props, ref) => {
       </button>
 
       <div
-        className='collapse navbar-collapse justify-content-end align-center w-auto'
+        className='collapse navbar-collapse justify-content-end align-items-center w-auto'
         id='main-nav'
         ref={navbarCollapseRef}
       >
@@ -103,10 +103,19 @@ const Header = forwardRef((props, ref) => {
           <li>
             <NavLink to='/ourBrands'>Nuestras marcas</NavLink>
           </li>
+          <li className='mx-auto'>
+            <NavLink to='/contacto' end>
+              Contacto
+              <i className='bi bi-headset ps-2' />
+            </NavLink>
+          </li>
         </ul>
       </div>
+
     </nav>
   )
 })
+
+Header.displayName = 'Header'
 
 export default Header
