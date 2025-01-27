534b5fd468c07987563764beb8f34eb1f7f708dd
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2025-01-27 20:46:36 +0400

    Updated content reader in user cases carousel

--- src/components/home/Success.jsx
+++ src/components/home/Success.jsx
@@ -17,7 +17,7 @@ const Success = () => {
       .then(response => response.json())
       .then(data => setContent(data.success))
       .catch(error => console.error('Error loading success cases:', error))
-  }, [])
+  }, [content])
 
   useEffect(() => {
     if (carouselInner.current) {
